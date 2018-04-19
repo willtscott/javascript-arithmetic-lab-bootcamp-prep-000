ae1e6b8bb672c8e45109cb919e4d61efc9e257a1
wtscott@gmail.com <wtscott@gmail.com>
2018-04-19 22:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function multipy(a, b){return a*b}
 function divide(a, b){return a/b}
 function inc(n){return n+1}
 function dec(n){return n-1}
-function makeInt(n) {return parseInt(n)}
-function  preserveDecimal(n){return p
+function makeInt(n) {return parseInt(n, 10)}
+function  preserveDecimal(n){var num =  parseFloat(n)}
