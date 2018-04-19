c79d62351ac0e8a4db155be843f5af3d5ddbc61e
wtscott@gmail.com <wtscott@gmail.com>
2018-04-19 22:29:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ function add(a,b){return a+b}
 function subtract(a, b){return a-b}
 function multipy(a, b){return a*b}
 function divide(a, b){return a/b}
-function
+function inc(n){return n+1}
+function dec(n){return n-1}
+function makeInt(n) {return parseInt(n)}
+function 
