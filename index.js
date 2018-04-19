acd7a390b9bdab7ce8bbca83ada8c60370b0bdac
wtscott@gmail.com <wtscott@gmail.com>
2018-04-19 22:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function divide(a, b){return a/b}
 function inc(n){return n+1}
 function dec(n){return n-1}
 function makeInt(n) {return parseInt(n)}
-function 
+function  preserveDecimal(n){return p
